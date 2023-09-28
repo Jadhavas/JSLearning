2c2f8cc0c8fec5a5e03aff5376eedcfca127eb23
Jadhavas <akshayjadhav0797@gmail.com>
2023-09-28 11:43:02 +0530

    update  variable Assign.js
    update  variable Assign.js

---
+++ 01_variable.js
@@ -0,0 +1,18 @@
+var firstName;// variable declaration
+firstName = "Akshay";// variable initialization
+console.log(firstName);
+firstName = "Mr Akshay"; // variable update
+console.log(firstName);
+
+var lastName = "Jadhav"; // variable declaration and initialization
+console.log(lastName);
+
+var height; // variable declaration
+console.log(height);
+height = "6"; //initialization
+console.log(height);
+height = "6.2"; // update
+console.log(height);
+
+var marks = '78%';
+console.log("10th marks;", marks);
---
+++ 01_variableAssign.js
@@ -0,0 +1,31 @@
+var myFullName;
+console.log(myFullName);
+
+var myFullName = "Akshay Jadhav";
+console.log(myFullName);
+myFullName = "Akshay Sambhaji Jadhav"
+console.log(myFullName);
+
+var myLove = "I Love JS";
+console.log(myLove);
+
+var firstName = "Akshay";
+console.log("firstName:",firstName);
+
+var lastName = "jadhav";
+console.log("lastName:",lastName);
+
+var age = "25";
+console.log("age:",age);
+
+var address = "Mangalwedha";
+console.log("address:",address);
+
+var pinCode = "413305";
+console.log("pinCode:",pinCode);
+
+var gender = "Male";
+console.log("gender:",gender);
+
+var isMarried = "No";
+console.log("isMarried:",isMarried);
